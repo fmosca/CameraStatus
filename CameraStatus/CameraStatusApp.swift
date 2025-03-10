e8db70f49586fb7fed640598c5f71f8d57785463
Francesco Mosca <fmosca@gmail.com>
2025-03-10 13:47:27 +0000

    Initial Commit

---
+++ CameraStatus/CameraStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraStatusApp.swift
+//  CameraStatus
+//
+//  Created by fra on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CameraStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraStatusTests/CameraStatusTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraStatusTests.swift
+//  CameraStatusTests
+//
+//  Created by fra on 10/03/2025.
+//
+
+import Testing
+@testable import CameraStatus
+
+struct CameraStatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
